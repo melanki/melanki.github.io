860574a085fdbbdb38aa5bfb530480279484e204
Marensius Bae Pettersen <marensius.pettersen@gmail.com>
2021-12-10 00:20:46 +0100

    draft

---
+++ src/routes/index.svelte
@@ -0,0 +1,9 @@
+<script>
+    import Hero from '../components/hero.svelte'
+    import Contact from '../components/contact.svelte'
+</script>
+
+<main class="grid">
+    <Hero />
+    <!-- <Contact /> -->
+</main>
